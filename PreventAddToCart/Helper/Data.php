955955be9ded0805bd1b945998b50cbd312c8593
inadeemkhan <khannadeem243@gmail.com>
2022-09-13 22:53:56 +0530

    Functionality update in observer.

--- PreventAddToCart/Helper/Data.php
+++ PreventAddToCart/Helper/Data.php
@@ -4,25 +4,89 @@ declare(strict_types=1);
 namespace Nadeem\PreventAddToCart\Helper;
 
 use Magento\Framework\App\Helper\AbstractHelper;
+use Magento\Framework\App\Http\Context;
+use Magento\Framework\App\Config\ScopeConfigInterface;
+use Magento\Store\Model\StoreManagerInterface;
 
 class Data extends AbstractHelper
 {
 
     /**
-     * @param \Magento\Framework\App\Helper\Context $context
+     * @var Context
+     */
+    private $_httpContext;
+    /**
+     * @var ScopeConfigInterface
+     */
+    private $_scopeConfig;
+    /**
+     * @var StoreManagerInterface
+     */
+    private $_storeManager;
+
+    /**
+     * @param Context $context
+     * @param Context $httpContext
+     * @param ScopeConfigInterface $scopeConfig
+     * @param StoreManagerInterface $storeManager
      */
     public function __construct(
-        \Magento\Framework\App\Helper\Context $context
+        \Magento\Framework\App\Helper\Context $context,
+        Context $httpContext,
+        ScopeConfigInterface $scopeConfig,
+        StoreManagerInterface $storeManager
     ) {
         parent::__construct($context);
+        $this->_httpContext = $httpContext;
+        $this->_scopeConfig = $scopeConfig;
+        $this->_storeManager = $storeManager;
     }
 
     /**
+     *
      * @return bool
      */
-    public function isEnabled()
+    public function isLoggedIn()
     {
-        return true;
+        $isLoggedIn = $this->_httpContext->getValue(
+            \Magento\Customer\Model\Context::CONTEXT_AUTH
+        );
+        return $isLoggedIn;
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isEnable(){
+        return $this->_scopeConfig->getValue(
+            "prevent_addtocart/general/is_enable",
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $this->_storeManager->getStore()->getStoreId()
+        );
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getErrorMessage(){
+        return $this->_scopeConfig->getValue(
+            "prevent_addtocart/general/error_message",
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $this->_storeManager->getStore()->getStoreId()
+        );
+    }
+
+    /**
+     *
+     * @return string
+     */
+    public function getRedirectUri(){
+        return $this->_scopeConfig->getValue(
+            "prevent_addtocart/general/redirect_uri",
+            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+            $this->_storeManager->getStore()->getStoreId()
+        );
     }
 }
-
--- PreventAddToCart/Observer/Frontend/Checkout/CartProductAddAfter.php
+++ PreventAddToCart/Observer/Frontend/Checkout/CartProductAddAfter.php
@@ -3,19 +3,73 @@ declare(strict_types=1);
 
 namespace Nadeem\PreventAddToCart\Observer\Frontend\Checkout;
 
-class CartProductAddAfter implements \Magento\Framework\Event\ObserverInterface
-{
+use Magento\Framework\Event\ObserverInterface;
+use Magento\Framework\Exception\LocalizedException;
+use Magento\Framework\Controller\ResultFactory;
+use Nadeem\PreventAddToCart\Helper\Data;
+use Magento\Framework\Message\ManagerInterface;
 
+class CartProductAddAfter implements ObserverInterface  
+{
     /**
-     * Execute observer
+     * @var Data
+     */
+    protected $_helper;
+    /**
+     * @var ManagerInterface;
+     */
+    protected $_messageManager;
+    
+    /**
+     * @param Data
+     * @param ManagerInterface
+     */
+    public function __construct(
+        Data $helper,
+        ManagerInterface $messageManager
+    ) {
+        $this->_helper = $helper;
+        $this->_messageManager = $messageManager;
+    }
+ 
+    /**
+     * customer register event handler
      *
      * @param \Magento\Framework\Event\Observer $observer
      * @return void
      */
-    public function execute(
-        \Magento\Framework\Event\Observer $observer
-    ) {
-        //Your observer code
+    public function execute(\Magento\Framework\Event\Observer $observer)
+    {
+        try{
+            $cancelCartStatus = $this->_helper->isEnable();
+            $cancelCartTmp = true;
+            if($cancelCartStatus && !$this->_helper->isLoggedIn()){
+                $cancelCartTmp = false;
+            }
+            if(!$cancelCartTmp){
+                $wording = $this->_helper->getErrorMessage();
+                $redirect = $this->_helper->getRedirectUri();
+                $this->_messageManager->addErrorMessage($wording);
+                $isAjax = $this->isAjax();
+                if($isAjax){
+                    $result['backUrl'] = $redirect;
+                    echo json_encode($result);
+                    exit;
+                }else{
+                    echo '<script>window.location.href = "'.$redirect.'";</script>';
+                    exit;
+                }
+            }
+        }catch(Exception $e){
+            throw new LocalizedException(__('Could not add Product into Cart.'));
+        }
+    }
+
+    /**
+     *
+     * @return bool
+     */
+    public function isAjax() {
+        return isset($_SERVER['HTTP_X_REQUESTED_WITH']) && $_SERVER['HTTP_X_REQUESTED_WITH'] === 'XMLHttpRequest';
     }
 }
-
